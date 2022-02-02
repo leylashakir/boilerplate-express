da9e5b80b13510d9124eb780f19a6f114f60e976
Glitch (clammy-silent-furniture) <none>
2022-02-02 16:58:09 +0000

    👵🚪 Checkpoint ./myApp.js:19135473/453

--- myApp.js
+++ myApp.js
@@ -3,8 +3,10 @@ var app = express();
 var dirName = "/app";
 
 app.get("/", (req, res) => {
-  res.sendFile(dirName + "/views/index.html");
+  res.sendFile(dirName.join("/views/index.html"));
 });
+
+app.use(express.static(dirName.join("/views")))
 
 console.log("Hello World")
 app.get("Hello Express")
