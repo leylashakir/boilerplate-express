4ad68049878bdff3ca172d17b3fb646f4e410fee
Glitch (clammy-silent-furniture) <none>
2022-02-02 18:39:42 +0000

    📟🏤 Checkpoint ./myApp.js:19135473/762

--- myApp.js
+++ myApp.js
@@ -2,18 +2,17 @@ var express = require('express');
 var app = express();
 var dirName = "/app";
 
-function middleware(req, res, next) {
-  if(res.status) {
-    console.log(req.method);
-    console.log(req.path);
-    console.log(req.ip);
-  }
-  
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
   next();
-  
-}
+});
 
-app.use(middleware());
+app.get("/now", function middleware(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, middleware(req, res) {
+        res.json({time: req.time})
+})
 
 var response = "Hello json";
 if (process.env.MESSAGE_STYLE === "uppercase") {
